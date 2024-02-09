e0cfd192b6a4be32b175d373e1624a38aa1bd75c
mnubhan <76425959+mnubhan@users.noreply.github.com>
2024-02-10 00:58:04 +0800

    Update answer.js

--- answer.js
+++ answer.js
@@ -3,3 +3,11 @@ const highestRank = arr =>{
   let valueArr = uniqueArr.map(x=>arr.filter(y=>y==x).length)
   return uniqueArr[valueArr.findLastIndex((x) => x == Math.max(...valueArr))];
 }
+
+function highestRank(arr) {
+  var getNum = num => arr.filter(n => n === num).length;
+  return arr.sort((a,b) => getNum(b) - getNum(a) || b - a)[0];
+}
+
+const highestRank = arr =>
+  arr.sort((a, b) => arr.filter(val => val === b).length - arr.filter(val => val === a).length || b - a)[0];
